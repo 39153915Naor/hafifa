6bfd61193ee709172e2c24ab64551b06723ef54c
39153915Naor <naoris100@gmail.com>
2025-09-16 22:58:10 +0300

    Create cartell.py

---
+++ Step1/cartell.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "CartellDB.txt")
+
+if not os.path.exists(DB_PATH):
+    with open(DB_PATH, "w") as f:
+        f.write("")
+
+  def get_vehicles():
