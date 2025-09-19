439fe4fd17a878aac065a115da874564f0360d99
39153915Naor <naoris100@gmail.com>
2025-09-19 12:35:19 +0300

    Update cartell.py

--- Step1/cartell.py
+++ Step1/cartell.py
@@ -29,5 +29,45 @@ def get_vehicle_by_id():
     print("not in list")
 
 
-        
+def add_vihicle():
+    car_num = input("add car num")
+    company = input("add car company")
+    color = input("add car color")
+    car_year = input("add car year")
+    car_km = input("add car km")
+
+    
+    if not car_num.isdigit():
+       print("Error: car num must be number")
+       return
+    elif not company.isalpha():
+       print("Error: company must be word")
+       return
+    elif not color.isalpha():
+        print("Error: color must be word")
+        return
+    elif not car_year.isdigit():
+        print("Error: car year must be number")
+        return
+    elif not car_km.isdigit():
+        print("Error: car km must be number")
+        return
+    with open(DB_PATH, "r") as f:
+        lines = f.readlines() 
+        for line in lines:
+            if line.startswith(car_num + " "):
+                print("Error: car in file")
+                return
+                
+    with open(DB_PATH, "a") as f:
+         f.write(car_num + " "company + " "color + " "car_year + " "car_km\n)
+         print("car added")
+
+
+
+
+
+
+
+
         
