aaf5c5438b3dfc1ebc4eabd0d88cbe40e60390b5
39153915Naor <naoris100@gmail.com>
2025-09-19 14:51:20 +0300

    Update cartell.py

--- Step1/cartell.py
+++ Step1/cartell.py
@@ -67,7 +67,7 @@ def add_vihicle():
 
 def main():
     tries = 0
-    while true:
+    while True:
         options = input("choose your choice\n 1: show all vehicles.\n 2: get vehicle by number.\n 3: add a new vehicle.\n type exit to quit")
         if options == "1":
             get_vehicles()
